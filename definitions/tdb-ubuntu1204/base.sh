7c7db3a880353e4349aebb2f6683a25e7b5db84d
Ben Langfeld <ben@langfeld.me>
2013-11-11 14:34:54 -0200

    Revert "Fix sudo for vagrant in ubuntu base box"
    This reverts commit bc2fad9d11770a36739ecb2a82fc1207a426c47b.

--- definitions/tdb-ubuntu1204/base.sh
+++ definitions/tdb-ubuntu1204/base.sh
@@ -2,5 +2,13 @@ apt-get -y update
 apt-get -y upgrade
 apt-get clean
 
+# Set up passwordless sudo for the vagrant user
+( cat <<'EOP'
+%vagrant ALL=NOPASSWD:ALL
+EOP
+) > /tmp/vagrant
+chmod 0440 /tmp/vagrant
+mv /tmp/vagrant /etc/sudoers.d/
+
 # Install NFS client for Virtualbox directory sharing
 apt-get -y install nfs-common
