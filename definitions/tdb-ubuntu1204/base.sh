cd2fa6907bc1c9a9fd6fbd9883054090db203464
Ben Langfeld <ben@langfeld.me>
2013-11-11 14:36:31 -0200

    Fix passwordless sudo for vagrant user

--- definitions/tdb-ubuntu1204/base.sh
+++ definitions/tdb-ubuntu1204/base.sh
@@ -4,7 +4,7 @@ apt-get clean
 
 # Set up passwordless sudo for the vagrant user
 ( cat <<'EOP'
-%vagrant ALL=NOPASSWD:ALL
+%vagrant ALL=(ALL) NOPASSWD:ALL
 EOP
 ) > /tmp/vagrant
 chmod 0440 /tmp/vagrant
