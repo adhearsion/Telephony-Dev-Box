b701d3bf45e2f07831cd76eae7852fc9bde6529c
Ben Langfeld <ben@langfeld.me>
2013-12-10 16:12:36 -0500

    Bump ruby versions
    Fixes #20

--- mojolingo-misc/recipes/adhearsion.rb
+++ mojolingo-misc/recipes/adhearsion.rb
@@ -5,7 +5,7 @@ include_recipe 'java'
 package 'libpcre3'
 package 'libpcre3-dev'
 
-%w{2.0.0-p247 jruby-1.7.4}.each do |ruby|
+%w{2.0.0-p353 jruby-1.7.9}.each do |ruby|
   rbenv_ruby ruby
 
   rbenv_gem 'bundler' do
--- mojolingo-misc/recipes/sippy_cup.rb
+++ mojolingo-misc/recipes/sippy_cup.rb
@@ -6,5 +6,5 @@ include_recipe 'rbenv::ruby_build'
 rbenv_ruby '1.9.3-p448'
 
 rbenv_gem 'sippy_cup' do
-  ruby_version '1.9.3-p448'
+  ruby_version '1.9.3-p484'
 end
