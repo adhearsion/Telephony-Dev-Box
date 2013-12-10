1eabf03c6f9e06d0d6339b09cb24858673c9ab5b
Ben Langfeld <ben@langfeld.me>
2013-12-10 17:45:53 -0500

    Fix load test box ruby version

--- mojolingo-misc/recipes/sippy_cup.rb
+++ mojolingo-misc/recipes/sippy_cup.rb
@@ -3,8 +3,10 @@ include_recipe 'wav2rtp'
 include_recipe 'rbenv::default'
 include_recipe 'rbenv::ruby_build'
 
-rbenv_ruby '1.9.3-p448'
+ruby_v = '1.9.3-p484'
+
+rbenv_ruby ruby_v
 
 rbenv_gem 'sippy_cup' do
-  ruby_version '1.9.3-p484'
+  ruby_version ruby_v
 end
