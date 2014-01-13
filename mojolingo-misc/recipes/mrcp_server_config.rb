972b9040d38e91f20a5c8c6b22e126a7a8dac0cf
Ben Langfeld <ben@langfeld.me>
2014-01-12 22:00:41 -0200

    Install UniMRCP Server init script
    #34

--- mojolingo-misc/recipes/mrcp_server_config.rb
+++ mojolingo-misc/recipes/mrcp_server_config.rb
@@ -4,3 +4,13 @@ cookbook_file "#{node['unimrcp']['install_dir']}/conf/unimrcpserver.xml" do
   source 'unimrcpserver.xml'
   mode 0644
 end
+
+bash "install_unimrcp_server_init_script" do
+  user "root"
+  cwd File.join(node['unimrcp']['src_dir'], 'unimrcp', 'build', 'init.d')
+  code 'cp unimrcp-server /etc/init.d'
+  action :nothing
+  subscribes :run, "bash[install_unimrcp]", :immediately
+end
+
+service 'unimrcp-server'
