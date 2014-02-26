514fabcd242171dabe528e95a18e2387383c3fff
Ben Langfeld <ben@langfeld.me>
2014-02-26 16:08:30 -0300

    We're not going to build our own generic base boxes any more, the Opscode Bento ones are better

--- definitions/tdb-centos64/virtualbox.sh
+++
@@ -1,9 +0,0 @@
-# Installing the virtualbox guest additions
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-cd /tmp
-mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
-yum -y remove gcc make gcc-c++ kernel-devel-`uname -r` perl
